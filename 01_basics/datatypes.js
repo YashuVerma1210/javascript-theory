18f1d73235b7e6f9a11023ef342df2372865bf2c
YashuVerma1210 <34019340+YashuVerma1210@users.noreply.github.com>
2024-04-28 14:33:00 +0000

    datatypes basics

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // it means treat all code as newer version of code
+
+// alert("Hello") // it won't work as code is running in node not browser
+
+// number -> 2^53 range
+// bigint
+// string --> "", ''
+// boolean
+// null - is a standalone value, representation of empty value
+// undefined - value not defined
+// symbol --? describes uniqeness
+
+// typeof null -> object
+// typeof undefined -> undefined
+
+// non primitive
+// object
