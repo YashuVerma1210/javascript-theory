36f071cad98e378aff209346098acd3f1658d679
YashuVerma1210 <34019340+YashuVerma1210@users.noreply.github.com>
2024-05-11 09:12:04 +0000

    reduce example

--- loops/forOf.js
+++ loops/forOf.js
@@ -1,4 +1,4 @@
-const numbers = [1,2,3,4,5,6]
+const numbers = [1, 2, 3, 4, 5, 6]
 // for of loop works on arrays and maps
 for (const number of numbers) {
     console.log(number);
@@ -6,9 +6,19 @@ for (const number of numbers) {
 
 // no duplicate values
 const countries = new Map()
-countries.set('IN','India')
-countries.set('USA','United States of America')
+countries.set('IN', 'India')
+countries.set('USA', 'United States of America')
 
 for (const [key, country] of countries) {
-    console.log(key , country);
+    console.log(key, country);
 }
+
+// reduce method 
+const str = "happy"
+
+const count = Array.from(str).reduce((acc, curr) => {
+    acc[curr] = acc[curr] ? acc[curr] + 1 : 1;
+    return acc
+}, {})
+
+console.log(count);
