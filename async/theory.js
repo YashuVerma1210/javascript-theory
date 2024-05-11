c4c6377c13bee63105920450a3d765380cdb6151
YashuVerma1210 <34019340+YashuVerma1210@users.noreply.github.com>
2024-05-11 12:40:22 +0000

    call stack, event loop

---
+++ async/theory.js
@@ -0,0 +1,33 @@
+// default behaviour
+// javascript is synchronous
+// js is single threaded
+
+// javascript engine is never used alone, always in runtime env
+
+
+// global execution context
+// lines are executed one by one
+// each operation waits for prev operation to complete
+
+// file read process
+// program can't read file
+// execution is transferred to kernel
+// kernel reads the file
+// excecution gets back to program
+// file read done
+
+// blocking code (sync code)
+// Ex - read file sync
+
+// non blocking code (async code)
+// Ex - read file async
+
+// JS Engine                      Web API/node - DOM API, Set Interval, SetTimeout, fetch
+//                                   Register Callback
+
+// JS Engine - Memory Heap and Call Stack
+
+// Task Queue
+// Promise task queue - priority queue
+
+// Event loop
